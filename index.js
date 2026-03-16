9cfc6a38dd277125711ff604325e37054960afbb
i-amDev <dev79037@gmail.com>
2026-03-16 11:29:52 +0530

    - Learned about reading file asynchronously, non-blocking way

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const fs = require('fs');
 
 const hello = 'Hello World!';
 
-console.log(hello);
+// console.log(hello);
 
+// Blocking, synchronous way
 const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textInput);
+// console.log(textInput);
 
 const textOutput = `This is what we know about avocado : ${textInput}. \nCreated on  ${Date.now()}`;
 
 fs.writeFileSync('./txt/output.txt', textOutput);
 
-console.log('File written!');
+// console.log('File written!');
+
+// Non-blocking, asynchronous way
+
+fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
+            console.log(data3);
+        });
+    });
+});
+console.log('Reading file...');
