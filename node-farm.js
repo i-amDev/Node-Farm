de090bce182250405c88afa4486d2caab75615bf
i-amDev <dev79037@gmail.com>
2026-03-16 17:30:28 +0530

    - Exported a custom created module replaceTemplate from replaceTemplate.js
    - Imported a custom created module in node-farm.js

--- node-farm.js
+++ node-farm.js
@@ -2,28 +2,14 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const templateCard= fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-};
 
 // Create a server
 const server = http.createServer((request, response) => {
