ddf34fee059a7c5a9587bdce8e516b961af8b0e9
i-amDev <dev79037@gmail.com>
2026-03-16 11:38:27 +0530

    - Learned about writing in a file asynchronously, non-blocking way

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
         console.log(data2);
         fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
             console.log(data3);
+            // Writing data in a file asynchronously.
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', error => {
+                console.log('File has been written!');
+            })
         });
     });
 });
