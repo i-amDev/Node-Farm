29c831c55ea3792ac607c1f9039bee96f24bd849
i-amDev <dev79037@gmail.com>
2026-03-16 10:13:05 +0530

    - Learned about how to write a file using Node.js

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ console.log(hello);
 const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log(textInput);
+
+const textOutput = `This is what we know about avocado : ${textInput}. \nCreated on  ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOutput);
+
+console.log('File written!');
